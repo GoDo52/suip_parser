21ea13ccba052141fd0e29d7041a21a2c1f123ae
Alex <txrushclash@gmail.com>
2024-04-22 21:33:02 +0300

    config updated

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = str(os.getenv("BOT_TOKEN2"))
+BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 ADMIN_ID_LIST = [int(admin) for admin in os.getenv("ADMIN_ID_LIST").split(',')]

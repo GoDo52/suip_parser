ebc0d284229d05a99a106f317badac4368ed0cde
Alex <txrushclash@gmail.com>
2024-04-25 02:37:24 +0300

    quick fix

--- database/db.py
+++ database/db.py
@@ -117,7 +117,7 @@ class Domain:
             c.executemany("INSERT INTO subdomains (domain, subdomain) VALUES (?, ?)",
                           [(self.domain, subdomain) for subdomain in subdomains])
             conn.commit()
-            return True, subdomains
+            return True, set(subdomains)
 
         new_subdomains_set = set(subdomains)
         if existing_subdomains == new_subdomains_set:

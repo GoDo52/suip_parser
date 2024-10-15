a00dccceeaa0d756a9051f781f03432d9cf7bd1e
alex <alex@gmail.com>
2024-10-15 19:10:53 +0000

    Revert "commit"
    This reverts commit 81ec024d1e6d411276bdf2803285469a930dccac.

--- scripts/parser.py
+++ scripts/parser.py
@@ -62,7 +62,7 @@ def is_proxy_working(proxy):
     bool: True if the proxy is working, False otherwise.
     """
     headers = {
-        "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36",
         'Accept': 'application/json',
         'Connection': 'keep-alive',
         'Accept-Encoding': 'gzip, deflate, br'
